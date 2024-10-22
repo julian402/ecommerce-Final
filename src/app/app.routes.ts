1842da2864d63829b39cca05f698191313463724
Julian402 <corredor.jul@hotmail.com>
2024-10-21 19:47:36 -0500

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,16 @@ import { HomeComponent } from './pages/home/home.component';
 import { ListproductComponent } from './pages/listproduct/listproduct.component';
 import { Component } from '@angular/core';
 import { DetailComponent } from './pages/detail/detail.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'products', component: ListproductComponent },
   { path: 'detail/:id', component: DetailComponent },
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
 ];
