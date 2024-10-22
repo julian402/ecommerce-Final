9e36fbf5b96c0825b8c79d75e33acba31ecccf11
Julian402 <corredor.jul@hotmail.com>
2024-10-21 19:42:01 -0500

    DetailPage

--- src/app/pages/detail/detail.component.ts
+++ src/app/pages/detail/detail.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Input, inject, signal } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import Product from '../../../../types/Product';
 
 @Component({
   selector: 'app-detail',
   standalone: true,
   imports: [],
   templateUrl: './detail.component.html',
-  styleUrl: './detail.component.css'
+  styleUrl: './detail.component.css',
 })
 export class DetailComponent {
+  productService = inject(ProductService);
+  @Input() id: string = '';
 
+  product = signal<null | Product>(null);
+
+  ngOnInit() {
+    console.log('Este es el detalle', this.id);
+    this.productService.getOneProduct(this.id).subscribe({
+      next: (response: any) => {
+        console.log(response);
+        this.product.set(response);
+      },
+      error: (error: any) => {
+        console.log(error);
+      },
+    });
+  }
 }
