42f0f06afc88003b1566a4f4a79d2272647196a1
Julian402 <corredor.jul@hotmail.com>
2024-10-21 19:43:06 -0500

    ListProdPage

--- src/app/pages/listproduct/listproduct.component.ts
+++ src/app/pages/listproduct/listproduct.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { CardProductComponent } from '../../components/card-product/card-product.component';
+import { ProductService } from '../../services/product.service';
+import Product from '../../../../types/Product';
 
 @Component({
   selector: 'app-listproduct',
   standalone: true,
-  imports: [],
+  imports: [CardProductComponent],
   templateUrl: './listproduct.component.html',
-  styleUrl: './listproduct.component.css'
+  styleUrl: './listproduct.component.css',
 })
 export class ListproductComponent {
+  productService = inject(ProductService);
+  products = signal<null | Product[]>(null);
 
+  ngOnInit() {
+    this.productService.getAllProducts().subscribe({
+      next: (response: any) => {
+        console.log(response);
+        this.products.set(response);
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
 }
