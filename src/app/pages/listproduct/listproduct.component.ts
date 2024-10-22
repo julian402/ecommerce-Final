e50225d47680712c3879a5902d722973fd27de1c
Julian402 <corredor.jul@hotmail.com>
2024-10-21 19:34:27 -0500

    v2Act

---
+++ src/app/pages/listproduct/listproduct.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listproduct',
+  standalone: true,
+  imports: [],
+  templateUrl: './listproduct.component.html',
+  styleUrl: './listproduct.component.css'
+})
+export class ListproductComponent {
+
+}
