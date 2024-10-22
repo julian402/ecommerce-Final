cb4e0cd18d351a3f8d47131971d5ba18d20a2748
Julian402 <corredor.jul@hotmail.com>
2024-10-21 19:44:39 -0500

    LoginPage

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { HeaderComponent } from '../../components/header/header.component';
+import { UserService } from '../../services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, HeaderComponent],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  private userService = inject(UserService);
+  private router = inject(Router);
 
+  loginForm = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.required],
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required],
+    }),
+  });
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      this.userService
+        .login(this.loginForm.value as { email: string; password: string })
+        .subscribe({
+          next: (response: any) => {
+            console.log(response);
+            localStorage.setItem('token', response.token);
+            this.router.navigate(['/']);
+          },
+          error: (error: any) => {
+            console.error(error);
+          },
+        });
+    }
+  }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,21 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  private http = inject(HttpClient);
+  constructor() {}
 
-  constructor() { }
+  register(formData: FormData) {
+    return this.http.post('http://localhost:3000/api/users', formData);
+  }
+
+  login(datosDelUsuario: { email: string; password: string }) {
+    return this.http.post(
+      'http://localhost:3000/api/users/login',
+      datosDelUsuario
+    );
+  }
 }
