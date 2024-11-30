dd6d298a85ce2dc23659d51efec29abc254c738a
Luis.Lozano <>
2024-11-30 12:09:14 -0500

    test product service

---
+++ src/app/services/product.service.spec.ts
@@ -0,0 +1,62 @@
+import { TestBed } from "@angular/core/testing";
+import { ProductService } from "./product.service";
+import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
+import { response } from "express";
+
+
+describe('ProductServiceTest',()=>{
+    let service: ProductService;
+    let httpMock: HttpTestingController;
+
+    beforeEach(()=>{
+        TestBed.configureTestingModule({
+            imports:[HttpClientTestingModule],
+            providers:[ProductService],
+        });
+        service = TestBed.inject(ProductService);
+        httpMock = TestBed.inject(HttpTestingController)
+    });
+
+    afterEach(()=>{
+        httpMock.verify()
+    });
+
+    it('Deberia hacer una llamada al endpoint getall y devolver el resultado',()=>{
+        //Arrange
+        const mockProductResponse = {}
+        
+        //Act & Assert
+        service.getAllProducts().subscribe(
+            response =>{
+                expect(response).toEqual(mockProductResponse)
+            }
+        )
+
+        //Assert - Segundo
+        const req = httpMock.expectOne('http://localhost:3000/api/products');
+
+        expect(req.request.method).toBe('GET');
+
+        req.flush(mockProductResponse)
+    })
+
+    it('Deberia hacer una llamada al endpoint getone y devolver el resultado',()=>{
+        //Arrange
+        const mockProductResponse = {}
+        const productId = '123145popk'
+        
+        //Act & Assert
+        service.getOneProduct(productId).subscribe(
+            response =>{
+                expect(response).toEqual(mockProductResponse)
+            }
+        )
+
+        //Assert - Segundo
+        const req = httpMock.expectOne('http://localhost:3000/api/products/'+productId);
+
+        expect(req.request.method).toBe('GET');
+
+        req.flush(mockProductResponse)
+    })
+})
